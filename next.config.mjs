dfc3debd7242c5be74d7ac307704a76ea5f98235
Senpai <clevercoder0307@gmail.com>
2025-06-17 13:51:35 +0530

    changed nextjs config file for fixing image issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: isProd ? '/Zodius' : '',
+  assetPrefix: isProd ? '/Zodius/' : '',
+  images: {
+    unoptimized: true, // Required for static hosting like GitHub Pages
+  },
+};
+
 
 export default nextConfig;
