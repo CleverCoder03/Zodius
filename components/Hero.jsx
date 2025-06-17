9d4a9c17030a7808f42a332c1ae43c192b7750c0
Senpai <clevercoder0307@gmail.com>
2025-06-17 15:47:55 +0530

    removed loader

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,26 +12,21 @@ gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
   const totalVideos = 4;
   const nextVideoRef = useRef(null);
 
   const handleVideoLoad = () => {
-    setLoadedVideos((prev) => prev +1);
+    setLoadedVideos((prev) => prev + 1);
   };
 
-  useEffect(() => {
-  setLoadedVideos(0);
-  setLoading(true);
-}, []);
-
-  useEffect(() => {
-    if (loadedVideos >= totalVideos -1 ) {
-      setLoading(false);
-    }
-  }, [loadedVideos]);
+  // useEffect(() => {
+  //   if (loadedVideos >= totalVideos - 1) {
+  //     setLoading(false);
+  //   }
+  // }, [loadedVideos]);
 
   const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
@@ -92,7 +87,7 @@ const Hero = () => {
 
   return (
     <>
-    {loading && (
+      {/* {loading && (
         <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
           <div className="three-body">
             <div className="three-body__dot"></div>
@@ -100,74 +95,74 @@ const Hero = () => {
             <div className="three-body__dot"></div>
           </div>
         </div>
-      )}
-    <div className="relative h-dvh w-screen overflow-x-hidden">
-      <div
-        id="video-frame"
-        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
-      >
-        <div>
-          <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div
-              onClick={handleMniVdClick}
-              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-            >
-              <video
-                src={getVideoSrc(upcomingVideoIndex)}
-                ref={nextVideoRef}
-                loop
-                muted
-                id="current-video"
-                className="size-64 origin-center scale-150 object-cover object-center"
-                onLoadedData={handleVideoLoad}
+      )} */}
+      <div className="relative h-dvh w-screen overflow-x-hidden">
+        <div
+          id="video-frame"
+          className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+        >
+          <div>
+            <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+              <div
+                onClick={handleMniVdClick}
+                className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+              >
+                <video
+                  src={getVideoSrc(upcomingVideoIndex)}
+                  ref={nextVideoRef}
+                  loop
+                  muted
+                  id="current-video"
+                  className="size-64 origin-center scale-150 object-cover object-center"
+                  onLoadedData={handleVideoLoad}
+                />
+              </div>
+            </div>
+            <video
+              src={getVideoSrc(currentIndex)}
+              ref={nextVideoRef}
+              loop
+              muted
+              id="next-video"
+              className="absolute-center invisible z-20 size-64 object-cover object-center"
+              onLoadedData={handleVideoLoad}
+            />
+            <video
+              src={getVideoSrc(
+                currentIndex === totalVideos - 1 ? 1 : currentIndex
+              )}
+              autoPlay
+              loop
+              muted
+              className="absolute left-0 top-0 size-full object-cover object-center"
+              onLoadedData={handleVideoLoad}
+            />
+          </div>
+
+          <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+            G<b>a</b>ming
+          </h1>
+          <div className="absolute left-0 top-0 z-40 size-full">
+            <div className="mt-24 px-5 sm:px-10">
+              <h1 className="special-font hero-heading text-blue-100">
+                redefi<b>n</b>e
+              </h1>
+              <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+                Enter the Metagame layer <br /> Unleash the Play Economy
+              </p>
+              <Button
+                id="watch-trailer"
+                title="Watch Trailer"
+                leftIcon={<TiLocationArrow />}
+                containerClass="bg-yellow-300 flex-center gap-1"
               />
             </div>
           </div>
-          <video
-            src={getVideoSrc(currentIndex)}
-            ref={nextVideoRef}
-            loop
-            muted
-            id="next-video"
-            className="absolute-center invisible z-20 size-64 object-cover object-center"
-            onLoadedData={handleVideoLoad}
-          />
-          <video
-            src={getVideoSrc(
-              currentIndex === totalVideos - 1 ? 1 : currentIndex
-            )}
-            autoPlay
-            loop
-            muted
-            className="absolute left-0 top-0 size-full object-cover object-center"
-            onLoadedData={handleVideoLoad}
-          />
         </div>
-
-        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
           G<b>a</b>ming
         </h1>
-        <div className="absolute left-0 top-0 z-40 size-full">
-          <div className="mt-24 px-5 sm:px-10">
-            <h1 className="special-font hero-heading text-blue-100">
-              redefi<b>n</b>e
-            </h1>
-            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-              Enter the Metagame layer <br /> Unleash the Play Economy
-            </p>
-            <Button
-              id="watch-trailer"
-              title="Watch Trailer"
-              leftIcon={<TiLocationArrow />}
-              containerClass="bg-yellow-300 flex-center gap-1"
-            />
-          </div>
-        </div>
       </div>
-      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        G<b>a</b>ming
-      </h1>
-    </div>
     </>
   );
 };
