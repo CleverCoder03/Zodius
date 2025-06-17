c2d25644a2b4a697d8842b5909dff0c24313f6a0
Senpai <clevercoder0307@gmail.com>
2025-06-17 13:04:58 +0530

    about section done

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import About from '@/components/About'
+import Hero from '@/components/Hero'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
+      <About />
+    </main>
+  )
+}
+
+export default page
